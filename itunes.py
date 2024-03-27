d4d9e683549c5fd7c7e37d261a207de25abad6c6
Nati <yohannesnatnael00@gmail.com>
2024-03-28 00:13:40 +0300

    second update

--- itunes.py
+++ itunes.py
@@ -7,4 +7,6 @@ if len(sys.argv[1]) == 2:
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
 
-print(json.dumps(response.json(), indent="2"))
+results = response.json()
+for result in results["results"]:
+	print(result["trackName"])
