b8ed2ed857c66f144a7bf7afed11ff11657aad73
Nati <yohannesnatnael00@gmail.com>
2024-03-28 00:01:09 +0300

    First Update

---
+++ itunes.py
@@ -0,0 +1,10 @@
+import sys
+import requests
+import json
+if len(sys.argv[1]) == 2:
+	sys.exit()
+
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+
+print(json.dumps(response.json(), indent="2"))
